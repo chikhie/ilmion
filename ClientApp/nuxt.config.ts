a2c992a4947529f9330dd5fd13f34869e51aee58
Seydin <chikhibra@hotmail.com>
2025-11-25 21:11:16 +0100

    update config

--- ClientApp/nuxt.config.ts
+++ ClientApp/nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   // Configuration de l'API
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://ilmanar.ibrahim-chikhi.cv:8080/api'
+      apiBase:'https://ilmanar.ibrahim-chikhi.cv:8080/api'
     }
   },
 
