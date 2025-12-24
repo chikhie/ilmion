93d2a52a0fd566e17913b1301850ab4b7c2303bf
Seydin <chikhibra@hotmail.com>
2025-12-24 19:40:01 +0100

    update dockerfile

--- ClientApp/build-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script pour modifier la configuration pendant le build
-
-# Remplacer l'URL de l'API dans nuxt.config.ts
-if [ -n "$NUXT_PUBLIC_API_BASE" ]; then
-  sed -i "s|apiBase:.*|apiBase: '${NUXT_PUBLIC_API_BASE}'|g" nuxt.config.ts
-  echo "Configuration mise à jour avec: ${NUXT_PUBLIC_API_BASE}"
-fi
-
